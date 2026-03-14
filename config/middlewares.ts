bab9c876a36f32c5cff8eacec8864921029edb2c
Jaganathan Kaliyaperumal <Jaganathan.Kaliyaperumal@maritz.com>
2026-03-13 20:38:53 -0500

    Initial Strapi CMS setup with Tournament and League content types

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,7 +4,18 @@ const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://post42golf.netlify.app',
+        'https://post42golf.com',
+        'http://localhost:5500',
+        'http://127.0.0.1:5500',
+      ],
+      methods: ['GET', 'HEAD'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
